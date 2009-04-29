e46454f86229965f50798adb6a7368c566e9409c
pyushkevich <pyushkevich>
2009-04-29 16:55:34 +0000

    *** empty log message ***

--- itkextras/itkGaussianInterpolateImageFunction.h
+++ itkextras/itkGaussianInterpolateImageFunction.h
@@ -3,8 +3,8 @@
   Program:   Insight Segmentation & Registration Toolkit
   Module:    $RCSfile: itkGaussianInterpolateImageFunction.h,v $
   Language:  C++
-  Date:      $Date: 2009/04/29 14:08:20 $
-  Version:   $Revision: 1.2 $
+  Date:      $Date: 2009/04/29 16:55:34 $
+  Version:   $Revision: 1.3 $
 
   Copyright (c) Insight Software Consortium. All rights reserved.
   See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
@@ -151,7 +151,11 @@ public:
     }
 
 protected:
-  GaussianInterpolateImageFunction(){};
+  GaussianInterpolateImageFunction()
+    {
+    for(size_t i = 0; i < VDim; i++)
+      order[i] = 0.0;
+    }
   ~GaussianInterpolateImageFunction(){};
   void PrintSelf(std::ostream& os, Indent indent) const
     { this->Superclass::PrintSelf(os,indent); }
